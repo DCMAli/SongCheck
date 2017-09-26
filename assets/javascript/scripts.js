578f654c92f6424ca324faf72a6f05c85e232643
Joyce Kim <joycekim@Joyces-MacBook-Pro.local>
2017-09-25 20:50:49 -0400

    still testing auth

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -12,24 +12,26 @@ var database = firebase.database();
 var provider = new firebase.auth.GoogleAuthProvider();
 var searchArray = [];
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+firebase.auth().signInWithRedirect(provider);
+
+// firebase.auth().getRedirectResult().then(function(result) {
+//   if (result.credential) {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     var token = result.credential.accessToken;
+//     // ...
+//   }
+//   // The signed-in user info.
+//   var user = result.user;
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
 
 // On submit, take user input and push to Firebase
 $("#submit-button").on("click", function(event) {
