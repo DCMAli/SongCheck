2d7c32bd8aa4a87c9ff689ae52a143b73234de4d
Joyce Kim <joycekim@Joyces-MacBook-Pro.local>
2017-09-25 20:41:31 -0400

    testing auth

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -12,16 +12,17 @@ var database = firebase.database();
 var provider = new firebase.auth.GoogleAuthProvider();
 var searchArray = [];
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
   // The signed-in user info.
   var user = result.user;
-  // ...
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
-  alert(errorCode);
   var errorMessage = error.message;
   // The email of the user's account used.
   var email = error.email;
