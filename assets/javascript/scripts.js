99c891cbf2800b059f4f04cd7b3e81d4ad3db643
Joyce Kim <joycekim@Joyces-MacBook-Pro.local>
2017-09-25 21:09:44 -0400

    still testing auth

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -11,29 +11,23 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var provider = new firebase.auth.GoogleAuthProvider();
 var searchArray = [];
-$(document).ready(function(){
-	firebase.auth().signInWithRedirect(provider);
+
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
 });
-
-
-// firebase.auth().getRedirectResult().then(function(result) {
-//   if (result.credential) {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     var token = result.credential.accessToken;
-//     // ...
-//   }
-//   // The signed-in user info.
-//   var user = result.user;
-// }).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-//   // ...
-// });
 
 // On submit, take user input and push to Firebase
 $("#submit-button").on("click", function(event) {
@@ -51,6 +45,5 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	var searchTerm = childSnapshot.val().searchItem;
 	var historyItem = $("<tr><td>" + searchTerm + "</td></tr>");
 	searchArray.push(searchTerm);
-	console.log(searchArray);
 	$("#insert-history-here").append(historyItem);
 });
